2925509158b6e059bc4af920be8a96239d4debbb
Ctorres2020 <cesarivantorres20@gmail.com>
2025-01-20 13:42:05 -0300

    ultimos cambios para deploy

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import swaggerUI from 'swagger-ui-express';
 import specs from './swagger/swagger.js';
 
-
+const port = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
 
@@ -13,10 +13,12 @@ app.use('/api/posts', postsRoutes);
 app.use('/api/users', usersRoutes);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
+// mongoose.connect("mongodb://localhost:27017", { dbName: 'demo_db'})
+
 mongoose.connect(process.env.MONGO_URL, { dbName: 'demo_db'})
     .then(() => {
         console.log('Connected to MongoDB')
-        app.listen(process.env.PORT, 'localhost', () => console.log('Server is running on http://localhost:4000'));
+        app.listen(port, 'localhost', () => console.log(`Server is running on http://localhost:${port}`));
     })
     .catch(err => console.log(err));
 
